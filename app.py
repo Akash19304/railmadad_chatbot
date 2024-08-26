a11e51e15052e2e748303f86f996bdf5037049b3
Akash19304 <123761006+Akash19304@users.noreply.github.com>
2024-08-26 23:25:47 +0530

    removed useless imports

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, HTTPException, UploadFile, File
-from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import base64
 from PIL import Image
